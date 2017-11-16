1edd6bdcfc51f78461f09df1f9fee77d2106c8bb
unknown <vicander_99@hotmail.com>
2017-11-16 18:14:32 -0500

    Animation improvements

--- src/sprites/Ted.js
+++ src/sprites/Ted.js
@@ -17,7 +17,7 @@ export default class extends Phaser.Sprite {
 
     this.walkSpeed = 1000
     this.jumpSpeed = 4000
-    this.iniJumpInc = 2000
+    this.iniJumpInc = 3000
     this.jumpInc = 250
 
     this.cursors = game.input.keyboard.createCursorKeys()
@@ -26,22 +26,28 @@ export default class extends Phaser.Sprite {
   setupAnimations () {
     this.animations.add('idle', [0, 0, 0, 0, 1, 2, 1, 0, 3, 4, 3, 0, 0, 0, 0, 0], 8, true)
     this.animations.add('walk', [16, 17, 18, 17, 16, 19, 20, 19], 16, true)
-    this.animations.add('jump', [32, 33, 34, 35, 36, 37, 38, 39], 32, false)
+    this.animations.add('jump', [32, 33, 34, 35, 36, 37, 38, 39, 40], 32, false)
+    this.animations.add('landing', [32, 33, 34, 35, 36, 37, 38, 39], 32, false)
     this.animations.add('crouch', [32, 38, 37, 48, 49, 50, 51, 52], 32, false)
     this.animations.add('stand-up', [52, 51, 50, 49, 48, 37, 38, 32], 32, false)
   }
 
   update () {
-    if (!this.airborne) { this.body.velocity.x = 0 }
 
-    if (this.body.touching.down) { this.airborne = false }
+    if (this.airborne) {
+      this.landingNeeded = true
+      this.frame = 40;
+    }
+    else { this.body.velocity.x = 0 }
+
+    this.airborne = this.body.touching.down? this.airborne = false : true;
 
     if ((this.goingUp && !this.cursors.up.isDown) || this.body.velocity.y <= -this.jumpSpeed) {
       this.goingUp = false
     }
-    if (this.goingUp && this.cursors.up.isDown) {
-      this.body.velocity.y -= this.jumpInc
-    }
+    // if (this.goingUp && this.cursors.up.isDown) {
+    //   this.body.velocity.y -= this.jumpInc
+    // }
     // walkSpeed = this.airborne? 1000:2000;
 
     if (!this.airborne && this.landingNeeded) { this.handleLandingAnimation() }
@@ -50,7 +56,7 @@ export default class extends Phaser.Sprite {
       if (!this.crouching) {
         if (this.cursors.left.isDown) {
           if (this.scale.x > 0) { this.scale.x *= -1 }
-          this.body.velocity.x = -this.walkSpeed
+            this.body.velocity.x = -this.walkSpeed
         } else if (this.cursors.right.isDown) {
           if (this.scale.x < 0) { this.scale.x *= -1 }
           this.body.velocity.x = this.walkSpeed
@@ -59,7 +65,6 @@ export default class extends Phaser.Sprite {
         if (!this.airborne) {
           if (this.cursors.up.isDown) {
             this.handleJumpAnimation()
-            this.body.velocity.y = -this.iniJumpInc
           } else if (this.cursors.left.isDown || this.cursors.right.isDown) {
             this.animations.play('walk')
           } else if (this.cursors.down.isDown) {
@@ -81,18 +86,18 @@ export default class extends Phaser.Sprite {
       this.animating = false
       this.airborne = true
       this.goingUp = true
-      this.landingNeeded = true
-      this.frame = 40
+      this.body.velocity.y = -this.iniJumpInc
     }, 250)
   }
 
   handleLandingAnimation () {
     this.animating = true
     this.landingNeeded = false
-    this.animations.play('jump')
+    this.animations.play('landing')
     setTimeout(() => {
       this.animating = false
-    }, 250)
+      this.frame = 0
+    }, 300)
   }
 
   handleCrouchAnimation () {
@@ -106,8 +111,8 @@ export default class extends Phaser.Sprite {
   }
 
   handleStandUpAnimation () {
-    this.crouching = false
     this.animating = true
+    this.crouching = false
     this.animations.play('stand-up')
     setTimeout(() => {
       this.animating = false
