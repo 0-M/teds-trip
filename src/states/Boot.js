afe81413cb0260c095fb95f83d1e76e5e97bffbe
mmarkman <mmarkman@users.noreply.github.com>
2017-11-15 22:29:33 -0500

    fixed conflict message

--- src/states/Boot.js
+++ src/states/Boot.js
@@ -26,10 +26,7 @@ export default class extends Phaser.State {
 
   preloadLevelAssets () {
     this.load.image('ground', './assets/images/ground.jpg')
-<<<<<<< HEAD
     this.load.image('floor2', './assets/images/floor2.png')
-=======
->>>>>>> zkrausman-dev
   }
 
   render () {
