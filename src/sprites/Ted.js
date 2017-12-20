c885a8b8d032650615244465b5573af677c85796
unknown <vicander_99@hotmail.com>
2017-12-20 17:05:19 -0500

    Dash animation
    Dodge is now dash. The proper animation and expected behavior have been achieved.
    
    Made moving while in the air easier. Ted moves left or right only as long as the user presses the left or right key.

--- src/sprites/Ted.js
+++ src/sprites/Ted.js
@@ -21,10 +21,11 @@ export default class extends Phaser.Sprite {
     this.jumpInc = 25       // Hold the jump button longer to jump higher.
 
     this.pointingRight = true
-    this.dodging = false
-    this.dodgeIni = 2000
-    this.dodgeVelDec = 50
-    this.dodgeLandingNeeded = false
+    this.dashing = false
+    this.dashIni = 3000
+    this.dashVelDec = 50
+    this.dashLandingNeeded = false
+    this.dashCoolingDown = false
 
     this.cursors = game.input.keyboard.addKeys({
       'up': Phaser.KeyCode.W,
@@ -32,48 +33,49 @@ export default class extends Phaser.Sprite {
       'left': Phaser.KeyCode.A,
       'right': Phaser.KeyCode.D,
       'flinch': Phaser.KeyCode.F,
-      'dodge': Phaser.KeyCode.Q
-      // 'attack': Phaser.KeyCode.C,
+      'dash': Phaser.KeyCode.Q
       })
     }
 
   setupAnimations () {
-    this.animations.add('idle', [0, 0, 0, 0, 1, 2, 1, 0, 3, 4, 3, 0, 0, 0, 0, 0], 8, true)
-    this.animations.add('walk', [16, 17, 18, 17, 16, 19, 20, 19], 16, true)
-    this.animations.add('jump', [32, 33, 34, 35, 36, 37, 38, 39, 40], 32, false)
+    this.animations.add(        'idle', [0, 0, 0, 0, 1, 2, 1, 0, 3, 4, 3, 0, 0, 0, 0, 0], 8, true)
+    this.animations.add(        'walk', [16, 17, 18, 17, 16, 19, 20, 19], 16, true)
+    this.animations.add(        'jump', [32, 33, 34, 35, 36, 37, 38, 39, 40], 32, false)
     this.animations.add('jump-landing', [32, 33, 34, 35, 36, 37, 38, 39], 32, false)
-    this.animations.add('crouch', [32, 38, 37, 48, 49, 50, 51, 52], 32, false)
-    this.animations.add('stand-up', [52, 51, 50, 49, 48, 37, 38, 32], 32, false)
-    this.animations.add('flinch', [64,65], 16, true)
-    this.animations.add('dodge', [81,82,83,83,83,83,83,83,83,83,83,83], 16, false)
-    // this.animations.add('dodge-landing1', [84,85,86,87], 16, false)
-    // this.animations.add('dodge-landing2', [48,37,38,0], 16, false)
+    this.animations.add(      'crouch', [32, 38, 37, 48, 49, 50, 51, 52], 32, false)
+    this.animations.add(    'stand-up', [52, 51, 50, 49, 48, 37, 38, 32], 32, false)
+    this.animations.add(      'flinch', [64,65], 16, true)
+    this.animations.add(        'dash', [81,82,83,83,83,83,83,83,83,83,83,83], 48, false)
   }
 
   update () {
 
-    if (this.airborne) {
-      this.landingNeeded = true
-      if (this.body.velocity.y < 0)  {this.frame = 40}
-      else                           {this.frame = 41}
+    if (!this.dashing){
+        this.body.velocity.x = 0
+        if (this.airborne) {
+            this.landingNeeded = true
+            if (this.body.velocity.y < 0) {
+                this.frame = 40
+            } else {
+                this.frame = 41
+            }
+        } else {
+            this.dashCoolingDown = false
+        }
     }
-    else                             { this.body.velocity.x = 0 }
 
     this.airborne = this.body.touching.down? this.airborne = false : true
 
     if ((this.goingUp && !this.cursors.up.isDown) || this.body.velocity.y <= -this.jumpSpeed) {
-      this.goingUp = false
+        this.goingUp = false
     }
     if (this.goingUp && this.cursors.up.isDown) {
-      this.body.velocity.y -= this.jumpInc
+        this.body.velocity.y -= this.jumpInc
     }
 
-    // if (this.dodgeLandingNeeded) {this.handleDodgeLandingAnimation()}
-    // if (!this.airborne && this.landingNeeded) {this.handleLandingAnimation()}
-
     if (!this.animating) {
-      if (this.cursors.dodge.isDown) {
-        this.handleDodgeAnimation()
+      if (this.cursors.dash.isDown && !this.dashCoolingDown) {
+        this.handleDashAnimation()
       // Left and right movement and sprite direction.
       } else if (!this.crouching) {
         if (this.cursors.left.isDown) {
@@ -154,27 +156,26 @@ export default class extends Phaser.Sprite {
     }, 500)
   }
 
-  handleDodgeAnimation () {
+  handleDashAnimation () {
     this.animating = true
-    this.body.velocity.x = this.pointingRight? -this.dodgeIni : this.dodgeIni
+    this.dashing = true
+    this.dashCoolingDown = true
+    this.body.velocity.x = this.pointingRight? -this.dashIni : this.dashIni
+    this.animations.play('dash')
     this.frame = 80
 
     setTimeout(() => {
-      this.dodging = true
-      this.animations.play('dodge')
-    }, 250)
-
-    setTimeout(() => {
       this.animating = false
-      this.dodging = false
+      this.dashing = false
+      // this.dashing = false
       this.body.velocity.x = 0
 
-    }, 750)
+    }, 250)
   }
 
-  handleDodgeLandingAnimation() {
+  handledashLandingAnimation() {
     this.animating = true
-    this.animations.play('dodge-landing')
+    this.animations.play('dash-landing')
     setTimeout(() => {
       this.animating = false
     }, 500)
